541424ea870b8f117ee576d35074f65d1d4fc0c0
Andrew Godfroy <andrew.godfroy@killerrin.com>
2016-05-12 23:08:23 -0400

    Initial Commit

---
+++ Portfolio/Scripts/Frontend.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $(".timeago").timeago();
+    $('input.color').colorPicker();
+
+    $(document).on('click', '.panel-heading span.clickable', function (e) {
+        var $this = $(this);
+        if (!$this.hasClass('panel-collapsed')) {
+            $this.parents('.panel').find('.panel-body').slideUp();
+            $this.addClass('panel-collapsed');
+            $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+        } else {
+            $this.parents('.panel').find('.panel-body').slideDown();
+            $this.removeClass('panel-collapsed');
+            $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+        }
+    })
+});
---
+++ Portfolio/Tools/DeployDB-Dev.bat
@@ -0,0 +1,2 @@
+..\..\packages\FluentMigrator.1.6.1\tools\Migrate.exe --db=mysql --target=..\bin\FotivAutomator.dll --configPath=..\Web.config -c=MainDB
+pause
