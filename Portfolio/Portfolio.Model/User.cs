11ef699c7c9a9573c7dd7bf9696e728e1014a55f
Andrew Godfroy <andrew.godfroy@killerrin.com>
2016-05-13 04:52:20 -0400

    Decided to role my own account system

--- Portfolio/Portfolio.Model/User.cs
+++ Portfolio/Portfolio.Model/User.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +14,11 @@ namespace Portfolio.Model
     {
         public int UserID { get; set; }
         public string Username { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+
+        [DataType(DataType.Password)]
         public string PasswordHash { get; set; }
 
         public virtual ICollection<Role> Roles { get; set; }
--- Portfolio/Portfolio.WebUI/Startup.cs
+++ Portfolio/Portfolio.WebUI/Startup.cs
@@ -8,7 +8,6 @@ namespace Portfolio.WebUI
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
